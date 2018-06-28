ef4b0335550d36b579756791c68e95e107ac267f
Viktor Hardubej <viktor.h@volpis.com>
2018-06-28 15:24:34 +0300

    show spinner in login form while request to firebase

--- src/App.js
+++ src/App.js
@@ -1,30 +1,65 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from "./components/common";
+import { Button, Header, Spinner } from "./components/common";
 import firebase from 'firebase';
 import LoginForm from "./components/LoginForm";
 
 export default class App extends Component {
+    state = { loggedIn: null };
+
   componentWillMount() {
-      const config = {
+      firebase.initializeApp({
           apiKey: "AIzaSyBji7T71j-E3grXhFRQlGQnz2eSU6EF3aE",
           authDomain: "auth-react-cbf3e.firebaseapp.com",
           databaseURL: "https://auth-react-cbf3e.firebaseio.com",
           projectId: "auth-react-cbf3e",
           storageBucket: "auth-react-cbf3e.appspot.com",
           messagingSenderId: "449649701681"
-      };
+      });
 
-      firebase.initializeApp(config);
+      firebase.auth().onAuthStateChanged((user) => {
+        if(user) {
+            this.setState({ loggedIn: true});
+        } else {
+            this.setState({ loggedIn: false});
+        }
+      });
+
+  }
+
+  renderContent() {
+      switch(this.state.loggedIn) {
+          case true:
+              return (
+                  <Button onPress={() => firebase.auth().}>
+                      Log Out
+                  </Button>
+          );
+          case false:
+              return <LoginForm />
+          default:
+              return (
+                  <View style={styles.spinnerStyle}>
+                      <Spinner size="large"/>
+                  </View>
+              );
+      }
   }
 
   render() {
     return (
       <View>
         <Header headerText={'Authentication'} />
-        <LoginForm />
+          {this.renderContent()}
       </View>
     );
   }
 }
+
+const styles = {
+    spinnerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+};
 
